22922be369fadcea2e7355d9afecdd5f958e9914
Rayhanul Islam Riaj <riaj2517@gmail.com>
2024-01-10 00:19:29 +0600

    Descriptive commit message

--- age-calculator/script.js
+++ age-calculator/script.js
@@ -1,5 +1,6 @@
 let userInput = document.getElementById('date');
 userInput.max = new Date().toISOString().split('T')[0];
+let result = document.getElementById('result');
 
 function CalculateAge() {
     let birthDate = new Date(userInput.value);
@@ -27,5 +28,17 @@ function CalculateAge() {
 
     if(d2 >= d1) {
         d3 = d2 - d1;
-    } else {}
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+    if(m3 < 0) {
+        m3 = 11;
+        y3--;
+    }
+    console.log(y3,m3,d3);
+    result.innerHTML = `You are <span>${y3}</span> years <span>${m3}</span> months and <span>${d3}</span> days old!`;
+}
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
 }
